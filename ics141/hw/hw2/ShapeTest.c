3f33605015109b970542259c440b0cd9edd77d5f
WillTran35 <williamtran807@gmail.com>
2025-10-15 13:21:23 -0700

    hw2

---
+++ ics141/hw/hw2/ShapeTest.c
@@ -0,0 +1,209 @@
+#include <malloc.h>
+#include <iostream>
+#include <string>
+using namespace std;
+
+typedef double (*double_method_type)(void *);
+typedef void (*void_method_type)(void *);
+typedef int (*int_method_type)(void *);
+// double_method_type → a pointer to a function that returns a double
+// void_method_type → a pointer to a function that returns void
+
+typedef union
+{
+    double_method_type double_method;   // for area()
+    void_method_type void_method_draw;  // for draw()
+    void_method_type void_method_print; // for print()
+    int_method_type int_method;         // for getter
+} VirtualTableEntry;
+
+typedef VirtualTableEntry *VTableType;
+
+#define AREA_INDEX 0
+#define DRAW_INDEX 2
+#define PRINT_INDEX 1
+#define GET_INDEX 3
+#define PI 3.14159
+
+struct Shape
+{
+    VTableType VPointer;
+    // double radius;
+    string name;
+};
+
+double Shape_area(Shape *_this)
+{
+    return 0.0;
+}
+
+void Shape_draw(Shape *_this)
+{
+    cout << "shape_draw" << endl;
+}
+
+void Shape_print(Shape *_this)
+{
+    cout << "shape_print" << endl;
+}
+
+VirtualTableEntry Shape_VTable[] = {
+    {.double_method = (double_method_type)Shape_area},
+    {.void_method_print = (void_method_type)Shape_print},
+    {.void_method_draw = (void_method_type)Shape_draw}};
+
+Shape *Shape_Shape(Shape *_this, string newName)
+{
+    _this->VPointer = Shape_VTable;
+    _this->name = newName;
+    return _this;
+};
+
+struct Circle
+{
+    VTableType VPointer;
+    int radius;
+};
+double Circle_area(Circle *_this)
+{
+    return PI * _this->radius * _this->radius;
+}
+
+void Circle_print(Circle *_this)
+{
+    cout << "Circle print" << endl;
+}
+
+void Circle_draw(Circle *_this)
+{
+    cout << "Circle draw" << endl;
+}
+
+VirtualTableEntry Circle_VTable[] = {
+    {.double_method = (double_method_type)Circle_area},
+    {.void_method_print = (void_method_type)Circle_print},
+    {.void_method_draw = (void_method_type)Circle_draw}};
+
+Circle *Circle(Circle *_this, string name, int radius)
+{
+    Shape_Shape((Circle *)_this, name);
+    _this->VPointer = Circle_VTable;
+    _this->radius = radius;
+    return _this;
+}
+
+struct Square
+{
+    VTableType VPointer;
+    int length;
+};
+
+double Square_area(Square *_this)
+{
+    return _this->length * _this->length;
+}
+
+void Square_print(Square *_this)
+{
+    cout << "Square print" << endl;
+}
+
+void Square_draw(Square *_this)
+{
+    cout << "Square draw" << endl;
+}
+
+int Square_get_length(Square *_this)
+{
+    return _this->length;
+}
+
+VirtualTableEntry Square_VTable[] = {
+    {.double_method = (double_method_type)Square_area},
+    {.void_method_print = (void_method_type)Square_print},
+    {.void_method_draw = (void_method_type)Square_draw},
+    {.int_method = (int_method_type)Square_get_length}};
+
+Square *Square_Square(Square *_this, string name, int length)
+{
+    Shape_Shape((Square *)_this, name);
+    _this->VPointer = Square_VTable;
+    _this->length = length;
+    return _this;
+}
+
+struct Rectangle
+{
+    VTableType VPointer;
+    int width;
+};
+
+double Rectangle_area(Rectangle *_this)
+{
+    int length = ((Square *)_this)->VPointer[GET_INDEX].int_method((Square *)_this);
+    return _this->width * length;
+}
+
+void Rectangle_print(Rectangle *_this)
+{
+    cout << "Rectangle print" << endl;
+}
+
+void Rectangle_draw(Rectangle *_this)
+{
+    cout << "Rectangle draw" << endl;
+}
+
+VirtualTableEntry Rectangle_VTable[] = {
+    {.double_method = (double_method_type)Rectangle_area},
+    {.void_method_print = (void_method_type)Rectangle_print},
+    {.void_method_draw = (void_method_type)Rectangle_draw},
+    {.int_method = (int_method_type)Square_get_length}};
+
+Rectangle *Rectangle_Rectangle(Rectangle *_this, string name, int length, int width)
+{
+    Square_Square((Square *)_this, name, length);
+    _this->VPointer = Rectangle_VTable;
+    _this->width = width;
+    return _this;
+}
+
+struct Triangle
+{
+    VTableType VPointer;
+    int length;
+    int height;
+};
+
+double Triangle_area(Triangle *_this, int length, int height)
+{
+    return 0.5 * length * height;
+}
+
+void Triangle_print(Triangle *_this)
+{
+    cout << "Triangle print" << endl;
+}
+
+void Triangle_draw(Triangle *_this)
+{
+    cout << "Triangle draw" << endl;
+}
+
+VirtualTableEntry Triangle_VTable[] = {
+    {.double_method = (double_method_type)Triangle_area},
+    {.void_method_print = (void_method_type)Triangle_print},
+    {.void_method_draw = (void_method_type)Triangle_draw}};
+
+Triangle *Triangle_Triangle(Triangle *_this, string name, int length, int height)
+{
+    Shape_Shape((Triangle *)_this, name);
+    _this->VPointer = Triangle_VTable;
+    _this->length = length;
+    _this->height = height;
+    return _this;
+}
+
+struct Picture
+{
+}
---
+++ ics141/hw/hw2/mainClass.java
@@ -0,0 +1,253 @@
+class Shape {
+
+    private String name;
+
+    // private int length; 
+    // private int width;
+    Shape(String name) {
+        // this.length = length;
+        // this.width = width;
+        this.name = name;
+    }
+
+    void print() {
+        // String result = "";
+        System.out.println("");
+    }
+
+    void draw() {
+        System.out.println();
+    }
+
+    double area() {
+        return 0;
+    }
+
+    String get_name() {
+        return this.name;
+    }
+}
+
+class Circle extends Shape {
+
+    private int radius;
+    static final double PI = 3.14;
+
+    Circle(String name, int radius) {
+        super(name);
+        this.radius = radius;
+    }
+
+    @Override
+    void print() {
+        // FirstTriangle(5, 5) : 12.5
+        String name = this.get_name();
+        String result = String.format("%s(%d) : %.2f", name, radius, area());
+        System.out.println(result);
+    }
+
+    @Override
+    double area() {
+        return PI * radius * radius;
+    }
+
+    @Override
+    void draw() {
+        System.out.println("   ***   ");
+        System.out.println(" *     * ");
+        System.out.println("*       *");
+        System.out.println(" *     * ");
+        System.out.println("   ***   ");
+    }
+}
+
+class Square extends Shape {
+
+    private int length;
+
+    Square(String name, int length) {
+        super(name);
+        this.length = length;
+    }
+
+    @Override
+    void print() {
+        String name = this.get_name();
+        String result = String.format("%s(%d) : %.2f", name, length, area());
+        System.out.println(result);
+    }
+
+    @Override
+    double area() {
+        return length * length;
+    }
+
+    @Override
+    void draw() {
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println("*****");
+        System.out.println("*****");
+    }
+
+    int get_length() {
+        return this.length;
+    }
+}
+
+class Rectangle extends Square {
+
+    private int width;
+
+    public Rectangle(String name, int length, int width) {
+        super(name, length);
+        this.width = width;
+    }
+
+    @Override
+    void print() {
+        String name = this.get_name();
+        String result = String.format("%s(%d, %d) : %.2f", name, this.get_length(), this.width, area());
+        System.out.println(result);
+    }
+
+    @Override
+    double area() {
+        return this.get_length() * width;
+    }
+
+    @Override
+    void draw() {
+        System.out.println("*********");
+        System.out.println("*       *");
+        System.out.println("*       *");
+        System.out.println("*       *");
+        System.out.println("*********");
+    }
+
+}
+
+class Triangle extends Shape {
+
+    private int height;
+    private int length;
+
+    Triangle(String name, int height, int length) {
+        super(name);
+        this.height = height;
+        this.length = length;
+
+    }
+
+    @Override
+    void print() {
+        String name = this.get_name();
+        String result = String.format("%s(%d, %d) : %.2f", name, height, length, area());
+        System.out.println(result);
+    }
+
+    @Override
+    double area() {
+        return 0.5 * height * length;
+    }
+
+    @Override
+    void draw() {
+        System.out.println("    *    ");
+        System.out.println("   ***   ");
+        System.out.println("  *****  ");
+        System.out.println(" ******* ");
+        System.out.println("*********");
+    }
+
+}
+
+class Picture {
+
+    ListNode head;
+
+    Picture() {
+        this.head = null;
+    }
+
+    void add(Shape sh) {
+        head = new ListNode(sh, head);
+    }
+
+    void printAll() {
+        ListNode copy = head;
+        while (copy != null) {
+            // System.out.print("hi");
+            copy.info.print();
+            copy = copy.next;
+        }
+    }
+
+    void drawAll() {
+        ListNode copy = head;
+        while (copy != null) {
+            copy.info.draw();
+            copy = copy.next;
+        }
+    }
+
+    double totalArea() {
+        double sum = 0;
+        ListNode copy = head;
+
+        while (copy != null) {
+            sum += copy.info.area();
+            copy = copy.next;
+        }
+        return sum;
+    }
+}
+
+class ListNode {
+
+    Shape info;
+    ListNode next;
+
+    ListNode(Shape info, ListNode next) {
+        this.info = info;
+        this.next = next;
+    }
+
+}
+
+public class mainClass {
+
+    public static void main(String[] args) {
+        int arg1 = Integer.parseInt(args[0]);
+        int arg2 = Integer.parseInt(args[1]);
+
+        Picture pic = new Picture();
+        Triangle t1 = new Triangle("FirstTriangle", arg1, arg2);
+        // System.out.println(t1.area());
+        ;
+        Triangle t2 = new Triangle("SecondTriangle", arg1 - 1, arg2 - 1);
+
+        Circle c1 = new Circle("FirstCircle", arg1);
+        Circle c2 = new Circle("SecondCircle", arg1 - 1);
+
+        Square s1 = new Square("FirstSquare", arg1);
+        Square s2 = new Square("SecondSquare", arg1 - 1);
+
+        Rectangle r1 = new Rectangle("FirstRectangle", arg1, arg2);
+        Rectangle r2 = new Rectangle("SecondRectangle", arg1-1, arg2-1);
+
+        pic.add(t1);
+        pic.add(t2);
+        pic.add(c1);
+        pic.add(c2);
+        pic.add(s1);
+        pic.add(s2);
+        pic.add(r1);
+        pic.add(r2);
+
+        pic.printAll();
+        pic.drawAll();
+        System.out.println("Total : " + pic.totalArea());
+    }
+}
