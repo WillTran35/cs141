338d03dbe0e2814978cf503d24b26802cf187442
WillTran35 <williamtran807@gmail.com>
2025-10-19 11:34:15 -0700

    hw2

--- ics141/hw/hw2/ShapeTest.c
+++ ics141/hw/hw2/ShapeTest.c
@@ -11,10 +11,10 @@ typedef int (*int_method_type)(void *);
 
 typedef union
 {
-    double_method_type double_method;   // for area()
-    void_method_type void_method_draw;  // for draw()
-    void_method_type void_method_print; // for print()
-    int_method_type int_method;         // for getter
+    double_method_type double_method; // for area()
+    void_method_type void_method;     // for draw()
+    // void_method_type void_method_print; // for print()
+    int_method_type int_method; // for getter
 } VirtualTableEntry;
 
 typedef VirtualTableEntry *VTableType;
@@ -49,8 +49,8 @@ void Shape_print(Shape *_this)
 
 VirtualTableEntry Shape_VTable[] = {
     {.double_method = (double_method_type)Shape_area},
-    {.void_method_print = (void_method_type)Shape_print},
-    {.void_method_draw = (void_method_type)Shape_draw}};
+    {.void_method = (void_method_type)Shape_print},
+    {.void_method = (void_method_type)Shape_draw}};
 
 Shape *Shape_Shape(Shape *_this, string newName)
 {
@@ -62,6 +62,7 @@ Shape *Shape_Shape(Shape *_this, string newName)
 struct Circle
 {
     VTableType VPointer;
+    string name;
     int radius;
 };
 double Circle_area(Circle *_this)
@@ -71,7 +72,8 @@ double Circle_area(Circle *_this)
 
 void Circle_print(Circle *_this)
 {
-    cout << "Circle print" << endl;
+    cout << _this->name << "("<<_this->radius << ") : " << Circle_area(_this) << endl;
+    // cout << std::format("{}({}) : {:.2f}", _this->name, _this->radius, Circle_area(_this)) << endl;
 }
 
 void Circle_draw(Circle *_this)
@@ -81,21 +83,24 @@ void Circle_draw(Circle *_this)
 
 VirtualTableEntry Circle_VTable[] = {
     {.double_method = (double_method_type)Circle_area},
-    {.void_method_print = (void_method_type)Circle_print},
-    {.void_method_draw = (void_method_type)Circle_draw}};
+    {.void_method = (void_method_type)Circle_print},
+    {.void_method = (void_method_type)Circle_draw}};
 
 Circle *Circle_Circle(Circle *_this, string name, int radius)
 {
     Shape_Shape((Shape *)_this, name);
     _this->VPointer = Circle_VTable;
     _this->radius = radius;
+    _this->name = name;
     return _this;
 }
 
 struct Square
 {
     VTableType VPointer;
+    string name;
     int length;
+    
 };
 
 double Square_area(Square *_this)
@@ -105,7 +110,8 @@ double Square_area(Square *_this)
 
 void Square_print(Square *_this)
 {
-    cout << "Square print" << endl;
+    // cout << "Square print" << endl;
+    cout << _this->name << "("<<_this->length << ") : " << Square_area(_this) << endl;
 }
 
 void Square_draw(Square *_this)
@@ -120,8 +126,8 @@ int Square_get_length(Square *_this)
 
 VirtualTableEntry Square_VTable[] = {
     {.double_method = (double_method_type)Square_area},
-    {.void_method_print = (void_method_type)Square_print},
-    {.void_method_draw = (void_method_type)Square_draw},
+    {.void_method = (void_method_type)Square_print},
+    {.void_method = (void_method_type)Square_draw},
     {.int_method = (int_method_type)Square_get_length}};
 
 Square *Square_Square(Square *_this, string name, int length)
@@ -129,13 +135,17 @@ Square *Square_Square(Square *_this, string name, int length)
     Shape_Shape((Shape *)_this, name);
     _this->VPointer = Square_VTable;
     _this->length = length;
+    // _this->name = name;
     return _this;
 }
 
 struct Rectangle
 {
     VTableType VPointer;
+    string name;
+    int length;
     int width;
+    
 };
 
 double Rectangle_area(Rectangle *_this)
@@ -146,7 +156,7 @@ double Rectangle_area(Rectangle *_this)
 
 void Rectangle_print(Rectangle *_this)
 {
-    cout << "Rectangle print" << endl;
+    cout << _this->name << "("<<_this->length << ", " << _this->width << ") : " << Rectangle_area(_this) << endl;
 }
 
 void Rectangle_draw(Rectangle *_this)
@@ -156,8 +166,8 @@ void Rectangle_draw(Rectangle *_this)
 
 VirtualTableEntry Rectangle_VTable[] = {
     {.double_method = (double_method_type)Rectangle_area},
-    {.void_method_print = (void_method_type)Rectangle_print},
-    {.void_method_draw = (void_method_type)Rectangle_draw},
+    {.void_method = (void_method_type)Rectangle_print},
+    {.void_method = (void_method_type)Rectangle_draw},
     {.int_method = (int_method_type)Square_get_length}};
 
 Rectangle *Rectangle_Rectangle(Rectangle *_this, string name, int length, int width)
@@ -165,24 +175,26 @@ Rectangle *Rectangle_Rectangle(Rectangle *_this, string name, int length, int wi
     Square_Square((Square *)_this, name, length);
     _this->VPointer = Rectangle_VTable;
     _this->width = width;
+    _this->length = length;
     return _this;
 }
 
 struct Triangle
 {
     VTableType VPointer;
+    string name;
     int length;
     int height;
 };
 
-double Triangle_area(Triangle *_this, int length, int height)
+double Triangle_area(Triangle *_this)
 {
-    return 0.5 * length * height;
+    return 0.5 * _this->length * _this->height;
 }
 
 void Triangle_print(Triangle *_this)
 {
-    cout << "Triangle print" << endl;
+    cout << _this->name << "("<<_this->length << ", " << _this->height << ") : " << Triangle_area(_this) << endl;
 }
 
 void Triangle_draw(Triangle *_this)
@@ -192,8 +204,8 @@ void Triangle_draw(Triangle *_this)
 
 VirtualTableEntry Triangle_VTable[] = {
     {.double_method = (double_method_type)Triangle_area},
-    {.void_method_print = (void_method_type)Triangle_print},
-    {.void_method_draw = (void_method_type)Triangle_draw}};
+    {.void_method = (void_method_type)Triangle_print},
+    {.void_method = (void_method_type)Triangle_draw}};
 
 Triangle *Triangle_Triangle(Triangle *_this, string name, int length, int height)
 {
@@ -201,27 +213,32 @@ Triangle *Triangle_Triangle(Triangle *_this, string name, int length, int height
     _this->VPointer = Triangle_VTable;
     _this->length = length;
     _this->height = height;
+    // _this->width = width;
+    // _this->name = name;
     return _this;
 }
 
-void printAll(Shape *arr, int length)
+void printAll(Shape *arr[], int length)
 {
+    // cout << "IN PRINT ALL" << endl;
     for (int i = 0; i < length; i++)
     {
-        arr[i]->VPointer[PRINT_INDEX].void_method(arr[i]);
+        (arr[i]->VPointer[PRINT_INDEX]).void_method(arr[i]);
     }
 }
 
-void drawAll(Shape *arr, int length)
+void drawAll(Shape *arr[], int length)
 {
+    // cout << "IN draw ALL" << endl;
     for (int i = 0; i < length; i++)
     {
         arr[i]->VPointer[DRAW_INDEX].void_method(arr[i]);
     }
 }
 
-double totalArea(Shape *arr, int length)
+double totalArea(Shape *arr[], int length)
 {
+    // cout << "IN area ALL" << endl;
     double result = 0;
     for (int i = 0; i < length; i++)
     {
@@ -232,13 +249,28 @@ double totalArea(Shape *arr, int length)
 
 int main(int argc, char *argv[])
 {
-    int arg1 = atoi(argv[0]);
-    int arg2 = atoi(argv[1]);
 
-    Shape *arr[1];
-    arr[0] = (Shape *)Triangle_Triangle((Triangle *)malloc(sizeof(Triangle)), "First Triangle", arg1, arg2);
+    int arg1 = atoi(argv[1]);
+    int arg2 = atoi(argv[2]);
 
-    printAll(arr, 1);
-    drawAll(arr, 1);
-    cout << totalArea(arr, 1) << endl;
+    Shape *arr[8] = {(Shape *)Triangle_Triangle((Triangle *)malloc(sizeof(Triangle)), "FirstTriangle", arg1, arg2),
+                     (Shape *)Triangle_Triangle((Triangle *)malloc(sizeof(Triangle)), "SecondTriangle", arg1 - 1, arg2 - 1),
+                     (Shape *)Circle_Circle((Circle *)malloc(sizeof(Circle)), "FirstCircle", arg1),
+                     (Shape *)Circle_Circle((Circle *)malloc(sizeof(Circle)), "SecondCircle", arg1 - 1),
+                     (Shape *)Square_Square((Square *)malloc(sizeof(Square)), "FirstSquare", arg1),
+                     (Shape *)Square_Square((Square *)malloc(sizeof(Square)), "SecondSquare", arg1 - 1),
+                     (Shape *)Rectangle_Rectangle((Rectangle *)malloc(sizeof(Rectangle)), "FirstRectangle", arg1, arg2),
+                     (Shape *)Rectangle_Rectangle((Rectangle *)malloc(sizeof(Rectangle)), "SecondRectangle", arg1 - 1, arg2 - 1)};
+    // Shape *arr[1];
+    // Triangle *t = (Triangle *)malloc(sizeof(Triangle));
+    // Triangle_Triangle(t, "First Triangle", arg1, arg2);
+
+    // arr[0] = (Shape *)t;
+    // cout << "im here" << endl;
+    // arr[0] =
+    // cout << "im here2" << endl;
+    printAll(arr, 8);
+    drawAll(arr, 8);
+    cout << "Total : " << totalArea(arr, 8) << endl;
+    return 0;
 }
